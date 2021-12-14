50fbd27da21e98c7e8c741206bab6591190f1d77
0K0 <qdouvillez@gmail.com>
2021-12-14 17:38:02 +0100

    :art: Main.vue added

---
+++ src/pages/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+    <Header />
+
+    <div class="hero">
+      <div class="hero__bloc">
+      </div>
+    </div>
+
+    <div class="preview">
+      <div class="preview__bloc"></div>
+      <div class="preview__bloc"></div>
+      <div class="preview__bloc"></div>
+    </div>
+
+    <div class="img">
+      <div class="img__bloc"></div>
+    </div>
+
+    <Footer />
+
+  </div>
+</template>
+
+<script>
+import "../assets/sass/main.scss";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default {
+  name: 'Main',
+  components: {
+    Header,
+    Footer,
+  }
+}
+</script>
